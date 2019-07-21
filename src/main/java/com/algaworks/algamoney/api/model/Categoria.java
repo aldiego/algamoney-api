9ce400154c263f9c73156ec45a2066396a6b0cfe
Diego Lima <diego.lima.aparecido@outlook.com>
2019-07-20 23:48:58 -0300

    Commit Inicial
    Adicionado a api criada para o curso da algaworks.

---
+++ src/main/java/com/algaworks/algamoney/api/model/Categoria.java
@@ -0,0 +1,19 @@
+package com.algaworks.algamoney.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long codigo;
+
+  private String nome;
+}
